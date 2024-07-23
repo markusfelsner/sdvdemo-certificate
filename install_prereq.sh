6d2239aaad0f8f180635c79959390fd2ca6c9740
markusfelsner <125271776+markusfelsner@users.noreply.github.com>
2024-07-23 14:41:07 +0200

    etc/hosts

--- install_prereq.sh
+++ install_prereq.sh
@@ -1,4 +1,5 @@
-sudo echo "test-app.com 127.0.0.1" > /etc/hosts
+sudo echo "test-app.com 192.168.49.2" >> /etc/hosts
+sudo echo "minikube.data.gov.au 127.0.0.1" >> /etc/hosts
 
 kubectl apply -f https://github.com/cert-manager/cert-manager/releases/download/v1.15.1/cert-manager.yaml
 
